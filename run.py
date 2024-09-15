d1e4108b13fbe8688b558a392e570003c311090e
aiccy <aiccyxixy@163.com>
2024-09-15 14:02:23 +0800

    1.0.4 添加了一些说明

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Description:
+import argparse
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8003, reload=True)
+    parser = argparse.ArgumentParser()
+    # 添加参数 port，设置默认值为 8080
+    parser.add_argument('-=', '--port', type=int, default=8080, help='Port number to run the server on (default: 8080)')
+    args = parser.parse_args()
+    uvicorn.run("app:app", host="0.0.0.0", port=args.port, reload=True)
