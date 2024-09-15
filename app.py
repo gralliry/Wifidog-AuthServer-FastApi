e0e6a749037c4d7ebb90e3ad95e50aea0c7d8108
aiccy <aiccyxixy@163.com>
2024-09-15 13:36:10 +0800

    1.0.2 修改了一下变动

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import sqlite3
 import uuid
 
 from fastapi import FastAPI, Request, Form, Query
-from fastapi.responses import RedirectResponse, JSONResponse
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 
@@ -49,22 +49,19 @@ MsgScriptPathFragment = "gw_message.php?"
 PingScriptPathFragment = "ping/?"
 AuthScriptPathFragment = "auth/?"
 
+GWAddress = "10.10.10.1"
+GWPort = "2060"
+GWId = "64644ADFE3CE"
 
-# 面向user
-@app.post(f"/register", response_class=JSONResponse)
-async def register(
-        username: str = Form(alias="username"),
-        password: str = Form(alias="password"),
-        ):
-    # 创建一个Cursor对象
-    cursor = conn.cursor()
-    cursor.execute(
-        'INSERT INTO info (username, password) VALUES (?, ?) ON DUPLICATE KEY UPDATE password = ?',
-        (username, password, password)
-    )
-    conn.commit()
-    cursor.close()
-    return JSONResponse(content={"status": "success"})
+
+def verify_server(gw_address: str = None, gw_port: str = None, gw_id: str = None):
+    if gw_address is not None and gw_address != GWAddress:
+        return False
+    if gw_port is not None and gw_port != GWPort:
+        return False
+    if gw_id is not None and gw_id != GWId:
+        return False
+    return True
 
 
 # 面向user
@@ -74,17 +71,13 @@ async def login_get(
         gw_address: str = Query(alias="gw_address"),
         gw_port: str = Query(alias="gw_port"),
         gw_id: str = Query(alias="gw_id"),
-        ip: str = Query(alias="ip"),
-        mac: str = Query(alias="mac"),
-        url: str = Query(alias="url"),
-        ):
-    request.session['gw_address'] = gw_address
-    request.session['gw_port'] = gw_port
-    request.session['gw_id'] = gw_id
-    request.session['ip'] = ip
-    request.session['mac'] = mac
-    request.session['url'] = url
-    return templates.TemplateResponse("login.html", {"request": request})
+        # ip: str = Query(alias="ip"),
+        # mac: str = Query(alias="mac"),
+        # url: str = Query(alias="url"),
+):
+    if not verify_server(gw_address=gw_address, gw_port=gw_port, gw_id=gw_id):
+        return f"当前页面({gw_address, gw_port, gw_id})不属于你处在的认证网络{GWAddress, GWPort, GWId}"
+    return templates.TemplateResponse("login.html", context={"request": request})
 
 
 # 面向user
@@ -93,11 +86,19 @@ async def login_post(
         request: Request,
         username: str = Form(alias="username"),
         password: str = Form(alias="password"),
-        ):
+        gw_address: str = Query(alias="gw_address"),
+        gw_port: str = Query(alias="gw_port"),
+        gw_id: str = Query(alias="gw_id"),
+        ip: str = Query(alias="ip"),
+        mac: str = Query(alias="mac"),
+        url: str = Query(alias="url"),
+):
+    if not verify_server(gw_address=gw_address, gw_port=gw_port, gw_id=gw_id):
+        return f"当前页面(login_post:{gw_address, gw_port, gw_id})不属于你处在的认证网络{GWAddress, GWPort, GWId}"
     # 创建一个Cursor对象
     cursor = conn.cursor()
     cursor.execute(
-        'SELECT 1 FROM info where username = ? and password = ?',
+        'SELECT id FROM user_info where username = ? and password = ?',
         (username, password)
     )
     # 提交事务
@@ -105,21 +106,18 @@ async def login_post(
     result = cursor.fetchone()
     if result is None:
         cursor.close()
-        return JSONResponse(content={"status": "fail", "message": "Password Wrong!"})
+        return templates.TemplateResponse("message.html",
+                                          context={'message': "账号不存在或密码错误", "request": request})
     # 更新用户信息
+    user_id = result[0]
     token = str(uuid.uuid4())
-    gw_address = request.session.get("gw_address", None)
-    gw_port = request.session.get("gw_port", None)
-    if gw_address is None or gw_port is None:
-        cursor.close()
-        return JSONResponse(content={"status": "fail", "message": "Cookies missing!"})
     cursor.execute(
-        'UPDATE info SET token = ? WHERE username = ? and password = ?',
-        (token, username, password)
+        'INSERT INTO connection (token, ip, mac, user_id) VALUES (?, ?, ?, ?)',
+        (token, ip, mac, user_id)
     )
     cursor.close()
     # 成功重定向
-    return RedirectResponse(url=f"http://{gw_address}:{gw_port}/wifidog/auth?token={token}", status_code=302)
+    return RedirectResponse(url=f"http://{GWAddress}:{GWPort}/wifidog/auth?token={token}", status_code=302)
 
 
 # 面向user
@@ -127,12 +125,10 @@ async def login_post(
 async def portal(
         request: Request,
         gw_id: str = Query(alias="gw_id"),
-        ):
-    url = request.session.get("url", None)
-    if url is None:
-        return templates.TemplateResponse("portal.html", context={'gw_id': gw_id})
-    else:
-        return RedirectResponse(url=url, status_code=302)
+):
+    if not verify_server(gw_id=gw_id):
+        return f"当前页面(portal:{gw_id})不属于你处在的认证网络{GWAddress, GWPort, GWId}"
+    return templates.TemplateResponse("portal.html", context={"request": request})
 
 
 # 面向user
@@ -140,7 +136,7 @@ async def portal(
 async def message(
         request: Request,
         msg: str = Query(alias="message"),
-        ):
+):
     return templates.TemplateResponse("message.html", context={'message': msg, "request": request})
 
 
@@ -152,7 +148,7 @@ async def ping(
         sys_memfree: str = Query(alias="sys_memfree"),
         sys_load: str = Query(alias="sys_load"),
         wifidog_uptime: str = Query(alias="wifidog_uptime"),
-        ):
+):
     # GET /wifidog/ping/?gw_id=64644ADFE3CE&sys_uptime=164&sys_memfree=26132&sys_load=0.30&wifidog_uptime=172
     # print({
     #     "gw_id": gw_id,
@@ -161,22 +157,28 @@ async def ping(
     #     "sys_load": sys_load,
     #     "wifidog_uptime": wifidog_uptime,
     # })
+    if not verify_server(gw_id=gw_id):
+        # 不回应非当前网络的请求
+        return None
     return "Pong"
 
 
 # 面向Wifidog
 @app.get(f"{Path}{AuthScriptPathFragment}"[:-1])
-async def auth(stage: str = Query(alias="stage"),
-               ip: str = Query(alias="ip"),
-               mac: str = Query(alias="mac"),
-               token: str = Query(alias="token"),
-               incoming: float = Query(alias="incoming"),
-               outgoing: float = Query(alias="outgoing"),
-               gw_id: str = Query(alias="gw_id"),
-               ):
+async def auth(
+        stage: str = Query(alias="stage"),
+        ip: str = Query(alias="ip"),
+        mac: str = Query(alias="mac"),
+        token: str = Query(alias="token"),
+        incoming: int = Query(alias="incoming"),
+        outgoing: int = Query(alias="outgoing"),
+        gw_id: str = Query(alias="gw_id"),
+):
+    if not verify_server(gw_id=gw_id):
+        return "Auth: 0"
     cursor = conn.cursor()
     cursor.execute(
-        'SELECT 1 FROM info where token = ?',
+        'SELECT 1 FROM connection where token = ?',
         (token,)
     )
     conn.commit()
@@ -185,7 +187,7 @@ async def auth(stage: str = Query(alias="stage"),
         cursor.close()
         return "Auth: 0"
     cursor.execute(
-        'UPDATE info SET ip = ?, mac = ?, incoming = ?, outgoing = ? WHERE token = ?',
+        'UPDATE connection SET ip = ?, mac = ?, incoming = ?, outgoing = ? WHERE token = ?',
         (ip, mac, incoming, outgoing, token)
     )
     conn.commit()
